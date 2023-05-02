d797e3f886dbe4198c4dcdc3a1b574f87f346810
sena-22 <senika_@naver.com>
2023-05-03 00:56:44 +0900

    feat: 타이틀 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import Search from './components/Search';
+import Search from './pages/SearchPage';
 
 const App = () => {
   return (
     <div>
+      <h1>국내 모든 임상시험 검색하고 온라인으로 참여하기</h1>
       <Search />
     </div>
   );
