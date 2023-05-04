1d77bebc2c96484a48b0278cc275c84aafa2690e
Hadam Cho <chohadam49@gmail.com>
2023-05-05 03:20:08 +0900

    fix: 검색어 일치 확인을 대소문자 구별하지 않도록 수정

--- src/components/DropdownItem.tsx
+++ src/components/DropdownItem.tsx
@@ -3,8 +3,8 @@ import SearchIcon from './SearchIcon';
 import * as S from './style';
 
 const DropdownItem = ({ keyword, isActive, onHover, onClick, children }: DropdownItemProps) => {
-  const splitter = new RegExp(`(${keyword})`, 'g');
-  const texts = children.split(splitter);
+  const keywordRegex = new RegExp(`(${keyword})`, 'gi');
+  const texts = children.split(keywordRegex);
 
   return (
     <S.Item className={isActive ? 'active' : ''} onMouseEnter={onHover} onClick={onClick}>
@@ -13,7 +13,7 @@ const DropdownItem = ({ keyword, isActive, onHover, onClick, children }: Dropdow
       </S.IconContainer>
       {texts.map((text, index) => {
         const key = text + index;
-        if (keyword === text) {
+        if (keywordRegex.test(text)) {
           return <S.SameWord key={key}>{text}</S.SameWord>;
         }
         return <S.Text key={key}>{text}</S.Text>;
