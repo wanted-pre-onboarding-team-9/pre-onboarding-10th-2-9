7a65712c71dae6a5653c6238bf7bd1ac65a398c3
kimisadev27 <kimisadev27@gmail.com>
2023-05-05 00:20:37 +0900

    refactor: 캐싱로직 수정

--- src/api/searchAPI.ts
+++ src/api/searchAPI.ts
@@ -1,29 +1,37 @@
 import instance from '.';
 
 import { RecommendedKeywords } from '../@types/search';
+import Cache from '../utils/cache';
 import { TEN_MINUTES_IN_MS } from '../utils/const';
-import storage from '../utils/storage';
+
+const cache = new Cache();
+
+type searchDataProp = {
+  data: RecommendedKeywords[];
+  expiration: number;
+};
 
 export const setSearchData = async (searchKeyword: string, data: RecommendedKeywords[]) => {
   const expiration = Date.now() + TEN_MINUTES_IN_MS;
 
-  const cachedKeyword = await storage.get(searchKeyword);
+  const cachedKeyword = await cache.get(searchKeyword);
   if (!cachedKeyword) {
     const maxlength = data.length > 8 ? 8 : data.length;
-    const sliceData = data.slice(0, maxlength);
+    // eslint-disable-next-line no-param-reassign
+    data = data.slice(0, maxlength);
 
-    storage.set(searchKeyword, { sliceData, expiration });
+    cache.set(searchKeyword, { data, expiration });
   }
 };
 
 export const getSearchData = async (searchKeyword: string) => {
-  const cachedKeyword = await storage.get(searchKeyword);
+  const cachedKeyword = await cache.get<searchDataProp>(searchKeyword);
 
   let cachedKeywordList;
 
   if (cachedKeyword) {
     if (cachedKeyword.expiration < Date.now()) {
-      storage.remove(searchKeyword);
+      cache.remove(searchKeyword);
     } else {
       cachedKeywordList = cachedKeyword.data;
     }
