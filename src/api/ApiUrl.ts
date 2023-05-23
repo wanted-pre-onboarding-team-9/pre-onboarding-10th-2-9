4278026b6a5f4e5c3278719d5361c8e601e7b10c
Hadam Cho <chohadam49@gmail.com>
2023-05-23 16:06:33 +0900

    chore: proxy → api로 변경

--- src/api/ApiUrl.ts
+++ src/api/ApiUrl.ts
@@ -1,5 +1,5 @@
-const BASE_URL = process.env.NODE_ENV === 'production' ? '/proxy' : '';
+const BASE_URL = process.env.NODE_ENV === 'production' ? '/api' : '';
 
 export const ApiUrl = Object.freeze({
-  SEARCH_KEYWORD: `${BASE_URL}search-conditions/?name=`,
+  SEARCH_KEYWORD: `${BASE_URL}/search-conditions/?name=`,
 });
