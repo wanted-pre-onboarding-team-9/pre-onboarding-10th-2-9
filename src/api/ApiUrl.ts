99f498ba8f1b36b5bedc3e067b9177f61fc7df02
Hadam Cho <chohadam49@gmail.com>
2023-05-23 15:53:52 +0900

    feat: proxy 설정

--- src/api/ApiUrl.ts
+++ src/api/ApiUrl.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://api.clinicaltrialskorea.com/api/v1/';
+const BASE_URL = process.env.NODE_ENV === 'production' ? '/proxy' : '';
 
 export const ApiUrl = Object.freeze({
   SEARCH_KEYWORD: `${BASE_URL}search-conditions/?name=`,
